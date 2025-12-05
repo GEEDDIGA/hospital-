60fdd07273283243ebf5d3e18ae5e1a3507826fd
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2025-12-05 19:17:48 +0300

    Create build_db.py

---
+++ build_db.py
@@ -0,0 +1,13 @@
+import os
+import django
+from django.contrib.auth.models import User
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings')
+django.setup()
+
+# Create superuser
+if not User.objects.filter(username='mustafa').exists():
+    User.objects.create_superuser('mustafa', 'mustafa@example.com', 'mustafa123@')
+    print('Superuser created successfully')
+else:
+    print('Superuser already exists')
