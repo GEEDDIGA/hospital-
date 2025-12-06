a85cce7e0ae853116318c6ee3d2f19d07ff3931b
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2025-12-07 00:08:08 +0300

    Disable DEBUG mode - fix completed

--- hospital/settings.py
+++ hospital/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-unsafe-key-change-in-production')
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'web-production-fb7ce.up.railway.app,hospital-gray-five.vercel.app,hospital-apweb-production-1ff3c.up.railway.app,p-zkm0.onrender.com,hospital-x5t5.onrender.com,127.0.0.1').split(',')
 CSRF_TRUSTED_ORIGINS = ['https://web-production-fb7ce.up.railway.app', 'https://hospital-x5t5.onrender.com']
 INSTALLED_APPS = [
