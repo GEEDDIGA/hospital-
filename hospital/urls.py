17e7b06e1497d2e1cf549ffd0ed01d77282d5524
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2025-12-07 01:06:05 +0300

    Update urls.py

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+        path('api/dashboard/', views.dashboard)
 ]
