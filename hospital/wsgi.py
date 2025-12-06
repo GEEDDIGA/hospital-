a47213a6d8a8b68c09bf447dc730c88b453a5cdc
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2025-12-07 00:38:38 +0300

    Remove problematic migration code from WSGI - revert to standard setup

--- hospital/wsgi.py
+++ hospital/wsgi.py
@@ -1,14 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital.settings')
-
-# Run migrations on application startup
-try:
-    call_command('migrate', verbosity=0)
-except Exception as e:
-    print(f"Migration error (non-fatal): {e}")
 
 application = get_wsgi_application()
 
