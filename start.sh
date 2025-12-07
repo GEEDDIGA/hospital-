5727dda9db3d047673210a6ff911fba1d0a81e6e
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2025-12-08 01:54:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,14 @@ set -o errexit
 
 # Run migrations
 python manage.py migrate
-python manage.py create_superuser
 
-# Collect static files  
+python manage.py shell << EOF
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+EOF
+
+# Collect static files
 python manage.py collectstatic --no-input
 
 # Start Gunicorn
