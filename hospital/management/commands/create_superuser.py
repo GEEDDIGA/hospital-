b454c35cd7e017c2c91de9e4892a8fe2fab8bc0b
GEEDDIGA <115898952+GEEDDIGA@users.noreply.github.com>
2025-12-07 19:02:59 +0300

    Update superuser default email to geeddiga@gmail.com

--- hospital/management/commands/create_superuser.py
+++ hospital/management/commands/create_superuser.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         username = os.getenv('SUPERUSER_USERNAME', 'admin')
-        email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
+        email = os.getenv('SUPERUSER_EMAIL', 'geeddiga@gmail.com')
         password = os.getenv('SUPERUSER_PASSWORD', 'admin123')
 
         # Delete existing user if exists
